b640845ed866e5cf6fdbe2d56483ae741c1f1ca1
andrmoe <mail@andreasmoe.com>
2025-02-18 13:20:59 +0100

    Automated compilation. Added references and paper structure

---
+++ dodo.py
@@ -0,0 +1,8 @@
+def task_compile_pdf():
+    latex_command = ['pdflatex', '-interaction=nonstopmode', '-output-directory=temp', 'timeinductivebias.tex']
+    return {'actions': [latex_command,
+                        ['bibtex', 'temp/timeinductivebias'],
+                        latex_command,
+                        latex_command],
+            'file_dep': ['timeinductivebias.tex', 'timeinductivebias.bib'],
+            'targets': ['temp/timeinductivebias.pdf']}
